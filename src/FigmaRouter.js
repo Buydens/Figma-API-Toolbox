57317469e12ef5fd1333f9ff134a69c51c4abdd3
Budyens <sam.buydens@protonmail.com>
2019-05-02 21:00:54 +0200

    Initial commit

---
+++ src/FigmaRouter.js
@@ -0,0 +1,50 @@
+"use strict";
+
+const token = process.env.VUE_APP_figmaApiKey;
+
+export default {
+
+/*
+    getFile: async function(figmaId){
+        return axios.get('https://api.figma.com/v1/files/' + figmaId, {
+            headers: {
+                'X-Figma-Token': token
+              }
+        }).then(response => {
+            return response.data;
+        });
+    },
+
+    getImageFills: async function(figmaId){
+        return axios.get('https://api.figma.com/v1/files/' + figmaId + "/images", {
+            headers: {
+                'X-Figma-Token': token
+              }
+        }).then(response => {
+            return response.data.meta.images;
+        });
+    },
+
+    getImage: async function(figmaId){
+        return axios.get('https://api.figma.com/v1/images/' + figmaId + "?scale=2&ids=0:1&format=svg", {
+            headers: {
+                'X-Figma-Token': token
+            },
+
+        }).then(response => {
+            return response.data;
+        });
+    },
+
+    getComponents: async function(figmaId){
+        return axios.get('https://api.figma.com/v1/components/' + figmaId, {
+            headers: {
+                'X-Figma-Token': token
+            },
+
+        }).then(response => {
+            return response.data;
+        });
+    }
+*/
+}
